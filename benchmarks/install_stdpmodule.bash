0ece5128e118ba21dd3c426bb287583ffe5e47bb
Teo Stocco <teo.stocco@epfl.ch>
2016-01-07 00:02:26 +0100

    move cluster scripts to new repo

--- benchmarks/install_stdpmodule.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~
-git clone https://github.com/zifeo/NEST-STDPModule.git stdpmodule
-cd stdpmodule
-./bootstrap.sh
-./configure --with-nest=/opt/nest/bin/nest-config
-make -j 16
-make install
